f1aafd3acc940e03ea5e7c8914950a3ddd2ae96c
vsudov <vanua.dubinkin@gmail.com>
2022-11-14 18:41:04 +0300

    Load code

---
+++ bub_sort/bub_sort.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, type;
+    setlocale(0, "");
+
+    cout << "Введите количество чисел: ";
+    cin >> n;
+
+    int* numbers = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        numbers[i] = rand() % 9 + 1;
+        cout << "Записали число в элемент массива под номером " << i + 1 << " число: " << numbers[i] << endl;
+    }
+
+    //Вывод чисел
+    cout << "Получаем ряд чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << numbers[i] << " ";
+    }
+
+    cout << endl;
+
+    // Сортировка
+    cout << "1 - по убывнию \n\
+2 - по возрастанию\n\
+Введите тип сортировки: ";
+
+    cin >> type;
+
+    switch (type)
+    {
+        case 1: {
+            for (int i = 0; i < n; i++) {
+                bool flag = true;
+                for (int j = 0; j < n - (i + 1); j++) {
+                    if (numbers[j] < numbers[j + 1]) {
+                        flag = false;
+                        swap(numbers[j], numbers[j + 1]);
+                    }
+                }
+                if (flag) {
+                    break;
+                }
+            }
+
+            //Вывод результата
+            cout << "Результат сортировки по убыванию: ";
+
+            for (int i = 0; i < n; i++) {
+                cout << numbers[i] << " ";
+            }
+
+            cout << endl;
+
+            break;
+        }
+        case 2: {
+            for (int i = 0; i < n; i++) {
+                bool flag = true;
+                for (int j = 0; j < n - (i + 1); j++) {
+                    if (numbers[j] > numbers[j + 1]) {
+                        flag = false;
+                        swap(numbers[j], numbers[j + 1]);
+                    }
+                }
+                if (flag) {
+                    break;
+                }
+            }
+
+            //Вывод результата
+            cout << "Результат сортировки по возрастанию: ";
+
+            for (int i = 0; i < n; i++) {
+                cout << numbers[i] << " ";
+            }
+
+            cout << endl;
+            break;
+        }
+        default: {
+            cout << "Неизвестный тип сортировки!";
+            break;
+        }
+    }
+    return 0;
+}
